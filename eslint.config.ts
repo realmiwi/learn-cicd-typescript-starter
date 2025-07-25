961228d6f24bb8b24b7ed779a09368f7bf10a84d
Michael Wildfeuer <michael@wildfeuer.dev>
2025-07-25 19:31:12 +0200

    fix

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,17 @@ import { defineConfig } from "eslint/config";
 import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.node,  // Changed from globals.browser to globals.node
+        ...globals.es2021
+      }
+    } 
+  },
+  ...tseslint.configs.recommended,
   pluginSecurity.configs.recommended,
 ]);
